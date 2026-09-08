a8a59f3be8d501296096b240d9a736ece827b626
RSkala <RichardSkala@gmail.com>
2026-09-07 20:22:20 -0700

    Added first pass player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    Rigidbody2D _rigidBody2D;
+
+    [SerializeField]
+    float _speed;
+
+    Vector2 _moveInput;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        _rigidBody2D.linearVelocity = _moveInput * _speed;
+    }
+
+    // We are using "Send Messages" in the PlayerInput component, so the method passes InputValue instead of the context
+    public void OnMove(InputValue inputValue)
+    {
+        //Debug.Log($"OnMove - inputValue: {inputValue}");
+
+        _moveInput = inputValue.Get<Vector2>();
     }
 }
